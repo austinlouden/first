3cb4b39685324bad0290710205c95f4ea47b82e7
Austin Louden <alouden@pinterest.com>
2016-03-26 19:59:46 -0700

    initial commit

--- First/AppDelegate.swift
+++ First/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let rootViewController = ViewController()
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
--- First/ViewController.swift
+++ First/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    let label: UILabel
+    let textField: UITextField
+    let button: UIButton
+    
+    init() {
+        label = UILabel()
+        label.text = "hello!"
+        
+        textField = UITextField()
+        textField.borderStyle = UITextBorderStyle.RoundedRect
+        
+        button = UIButton(type: .RoundedRect)
+        button.setTitle("Set Default Label Text", forState: UIControlState.Normal)
+
+        super.init(nibName: nil, bundle: nil)
+        
+        textField.delegate = self
+        button.addTarget(self, action: "buttonPressed", forControlEvents: .TouchUpInside)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        label.frame = CGRectMake(20, 40, CGRectGetWidth(self.view.frame) - 40, 20)
+        self.view.addSubview(label)
+        
+        textField.frame = CGRectMake(20, CGRectGetMaxY(label.frame) + 10, CGRectGetWidth(self.view.frame) - 40, 30)
+        self.view.addSubview(textField)
+        
+        button.frame = CGRectMake(20, CGRectGetMaxY(textField.frame) + 10, CGRectGetWidth(self.view.frame) - 40, 30)
+        self.view.addSubview(button)
+        
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        label.text = textField.text
+        return false
+    }
+    
+    func buttonPressed() {
+        label.text = "hello!"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
